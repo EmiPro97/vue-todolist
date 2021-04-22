324252beda8f9483309ae22bc562429bcfebe921
EmiPro97 <emi.proie97@gmail.com>
2021-04-23 01:24:49 +0200

    added update tasks function

--- js/main.js
+++ js/main.js
@@ -3,17 +3,21 @@ const app = new Vue({
 
     data: {
         todos: [
-            {text: 'Fare la spesa', completed: false,},
-            {text: 'Seguire la lezione', completed: false,},
-            {text: 'Punchare Checco', completed: false,},
+            {text: 'Fare la spesa', completed: false, visibility: false,},
+            {text: 'Seguire la lezione', completed: false, visibility: false,},
+            {text: 'Punchare Checco', completed: false, visibility: false,},
         ],
         newTask: '',
+        editTask: {
+            text: '',
+            index: null,
+        },
     },
 
     methods: {
         // Add a new task
         addTask() {
-            // Validation
+            // Validation and push the new task
             if (this.newTask != '' && this.newTask != null) {
                 this.todos.push({text: this.newTask, completed: false});
 
@@ -32,5 +36,27 @@ const app = new Vue({
         updateCompletedKey(index) {
             this.todos[index].completed = !this.todos[index].completed;
         },
+
+        // Enter in edit mode
+        showEdit(index) {
+            this.editTask.text = this.todos[index].text;
+            this.editTask.index = index;
+            this.todos[index].visibility = true;
+        },
+
+        // Update tasks in edit mode
+        updateTask(index) {
+            // Validation and update the selected task
+            if (this.editTask.text != '' && this.editTask != null) {
+                this.todos[this.editTask.index].text = this.editTask.text;
+            }
+
+            // Reset
+            this.editTask.text = '';
+            this.editTask.index = null;
+            this.todos[index].visibility = false;
+        },
+
+
     },
 });
